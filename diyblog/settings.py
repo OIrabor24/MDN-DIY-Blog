fda2589e291d02e71e48092cee1caa54a5ddc685
Osahon (Sean) Irabor <93814062+OIrabor24@users.noreply.github.com>
2021-11-18 18:26:12 -0800

    Update settings.py
    removed secret key!

--- diyblog/settings.py
+++ diyblog/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#zui-gq4b#+t(g*i#kypo9^y18q7l6)vplbgo+58mua29*g9p&'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
